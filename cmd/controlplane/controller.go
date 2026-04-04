ed0eef767c10cc56e384de44242921a9fd513744
Diego Caspi <diego.caspi@proton.me>
2026-04-04 21:08:46 +0200

    chore: use indexer to filter by uid
    Signed-off-by: Diego Caspi <diego.caspi@proton.me>

--- cmd/controlplane/controller.go
+++ cmd/controlplane/controller.go
@@ -15,6 +15,7 @@ import (
 	"github.com/orray-proj/orray/pkg/controller/canvas"
 	"github.com/orray-proj/orray/pkg/controller/layer"
 	"github.com/orray-proj/orray/pkg/kubernetes"
+	"github.com/orray-proj/orray/pkg/kubernetes/indexer"
 	versionpkg "github.com/orray-proj/orray/pkg/version"
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -82,7 +83,7 @@ func (c *controller) run(ctx context.Context) error {
 }
 
 // setupControllerManager sets up the controller manager.
-func (c *controller) setupControllerManager(_ context.Context) (manager.Manager, error) {
+func (c *controller) setupControllerManager(ctx context.Context) (manager.Manager, error) {
 	logger := c.Logger
 
 	logger.Debug("loading in-cluster REST config")
@@ -139,7 +140,11 @@ func (c *controller) setupControllerManager(_ context.Context) (manager.Manager,
 		return nil, fmt.Errorf("failed to create controller manager: %w", err)
 	}
 
-	// TODO: add indexer
+	// Register Indexers
+	if err := indexer.NewCanvasIndexer(mgr).Index(ctx); err != nil {
+		return nil, fmt.Errorf("failed to register canvas indexer: %w", err)
+	}
+
 	return mgr, nil
 }
 
--- pkg/kubernetes/indexer/indexer.go
+++ pkg/kubernetes/indexer/indexer.go
@@ -1,8 +1,49 @@
 package indexer
 
-import "context"
+import (
+	"context"
 
-// Indexer is an interface for indexing resources.
-type Indexer interface {
-	Index(ctx context.Context) error
+	"github.com/orray-proj/orray/api/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+const (
+	// IndexCanvasID is the index name for Canvas UID.
+	IndexCanvasID = "metadata.uid"
+	// IndexLayerID is the index name for Layer UID.
+	IndexLayerID = "metadata.uid"
+)
+
+// CanvasIndexer indexes Canvas and Layer resources.
+type CanvasIndexer struct {
+	mgr manager.Manager
+}
+
+// NewCanvasIndexer creates a new CanvasIndexer.
+func NewCanvasIndexer(mgr manager.Manager) *CanvasIndexer {
+	return &CanvasIndexer{
+		mgr: mgr,
+	}
+}
+
+// Index implements the Indexer interface.
+func (i *CanvasIndexer) Index(ctx context.Context) error {
+	if err := i.mgr.GetFieldIndexer().IndexField(ctx, &v1alpha1.Canvas{}, IndexCanvasID, func(rawObj client.Object) []string {
+		canvas := rawObj.(*v1alpha1.Canvas)
+		if canvas.UID == "" {
+			return nil
+		}
+		return []string{string(canvas.UID)}
+	}); err != nil {
+		return err
+	}
+
+	return i.mgr.GetFieldIndexer().IndexField(ctx, &v1alpha1.Layer{}, IndexLayerID, func(rawObj client.Object) []string {
+		layer := rawObj.(*v1alpha1.Layer)
+		if layer.UID == "" {
+			return nil
+		}
+		return []string{string(layer.UID)}
+	})
 }
